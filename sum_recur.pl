7283e6689f13897ef221ea065a84460c215f4b58
Kyle <kyle.stratis@gmail.com>
2017-10-18 20:19:19 -0400

    Exercise 4.2

--- sum_recur.pl
+++ sum_recur.pl
@@ -9,10 +9,26 @@ sub sum_recur {
     if (scalar @array == 1) {
         return $array[0];
     }
-    else {
-        return $array[0] + sum_recur(arr => [@array[1..$#array]]);
-    }
+    
+    return $array[0] + sum_recur(arr => [@array[1..$#array]]);
 }
 
 my $x = sum_recur(arr => [1, 2, 3, 4]);
-print "$x\n";
+print "My answer to 4.1: $x\n";
+
+# Exercise 4.2
+# Write recursive fxn to count number of items in list
+
+sub count_list {
+    my %inputs = @_;
+    my @array = @{$inputs{arr}};
+
+    if (scalar @array == 0) {
+        return 0;
+    }
+    
+    return 1 + count_list(arr => [@array[1..$#array]]);
+}
+
+$x = count_list(arr => [1, 2, 3, 4]);
+print "My answer to 4.2: $x\n";
--- sum_recur.py
+++ sum_recur.py
@@ -18,3 +18,16 @@ def sum_book(list):
 
 x = sum_book([2,4,6])
 print "{}{}".format("Book answer to 4.1: ", x)
+
+# Exercise 4.2
+# Write recursive fxn to count number of items in list
+
+def list_count(list):
+    if list == []:
+        return 0
+    return 1 + list_count(list[1:])
+
+x = list_count([2,4,6])
+print "{}{}".format("My answer to 4.2: ", x)
+
+# Book answer to 4.2 is identical to mine
